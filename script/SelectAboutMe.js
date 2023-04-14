62899fb7191872485e25f3a69b67e0fe834fc5e9
Rafael Menezes <faelgmp@gmail.com>
2023-04-14 15:06:12 -0300

    sobre finalizado

--- script/SelectAboutMe.js
+++ script/SelectAboutMe.js
@@ -1,4 +1,5 @@
 function selectAboutMe(value){
+    const setTime = 300;
     const arrText = document.querySelectorAll(".size-selection-about-me__text");
     const arrButton = document.querySelectorAll(".size-selection-about-me__button");
 
@@ -9,20 +10,26 @@ function selectAboutMe(value){
         };
     };
 
-    const animationText = (add) => {
-        if(add){
-            
-        }else{
-
-        };
+    const addClass = (element, time) =>{
+        element.style.opacity = "0";
+        setTimeout(() => element.classList.add("size-selection-about-me__text--off"), time);        
+    };
+    const removeClass = (element, time) =>{
+        element.classList.remove("size-selection-about-me__text--off");
+        setTimeout(() => element.style.opacity = "1", time);        
     };
 
     if(value == 0){
-
+        addClass(arrText[0], setTime);
+        addClass(arrText[1], setTime);
     }else if(value == 1){
-
+        addClass(arrText[0], setTime);
+        removeClass(arrText[1], setTime);
     }else{
-
+        removeClass(arrText[0], setTime);
+        removeClass(arrText[1], setTime);
     }
     selectButton(value);
 };
+
+selectAboutMe(0)

63bf0922bbeddc7e652753642b83143c13051cf5
Rafael Menezes <faelgmp@gmail.com>
2023-04-15 14:55:16 -0300

    screen project finalizado

--- script/ShowScreenProjects.js
+++ script/ShowScreenProjects.js
@@ -1,22 +1,14 @@
 const arrButton = document.querySelectorAll(".project__description .button");
 const arrScreen = document.querySelectorAll(".screen-project");
 
-const time = 300;
-
 for(let contButton = 0; contButton < arrButton.length; contButton++){
-    arrButton[contButton].setAttribute("onclick", `showScreenProjects(true, ${contButton})`);
-    //arrScreen[contButton].setAttribute("onmouseout", `showScreenProjects(true, ${contButton})`);
-};
+    arrButton[contButton].addEventListener("click", () =>{
+        arrScreen[contButton].classList.add("screen-project--on-display");
+        setTimeout(() => arrScreen[contButton].classList.add("screen-project--on-scale"), 0);
+    });
 
-function showScreenProjects(value, element, ){
-    if(value){
-        arrScreen[element].classList.add("screen-project--on-display");
-        setTimeout(() => arrScreen[element].classList.add("screen-project--on-scale"), time);
-
-        //setTimeout(() => showScreenProjects(false, element), 10*time);
-
-    }else{
-        arrScreen[element].classList.remove("screen-project--on-scale");
-        setTimeout(() => arrScreen[element].classList.remove("screen-project--on-display"), time);
-    }
+    arrScreen[contButton].addEventListener("mouseleave", () => {
+        arrScreen[contButton].classList.remove("screen-project--on-scale");
+        setTimeout(() => arrScreen[contButton].classList.remove("screen-project--on-display"), 300);
+    });
 };
